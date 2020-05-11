4627f3fd31358089b9b5a9561bf784af733630ed
Andrew Watkins <andrew@groat.nz>
2020-05-12 07:33:55 +1200

    add docs folder

---
+++ docs/sw.js
@@ -0,0 +1,145 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "/_next/static/36au9bOQHhHNjShwqe0Ly/_buildManifest.js",
+    "revision": "fb96ae7926f5104f50f0cf1b3a23a9b5"
+  },
+  {
+    "url": "/_next/static/36au9bOQHhHNjShwqe0Ly/_ssgManifest.js",
+    "revision": "abee47769bf307639ace4945f9cfd4ff"
+  },
+  {
+    "url": "/_next/static/36au9bOQHhHNjShwqe0Ly/pages/_app.js",
+    "revision": "7f34e037db2ad880864c26c3b63578c0"
+  },
+  {
+    "url": "/_next/static/36au9bOQHhHNjShwqe0Ly/pages/_error.js",
+    "revision": "349ad5fae83ab7ccc8dfb49f3254e02d"
+  },
+  {
+    "url": "/_next/static/36au9bOQHhHNjShwqe0Ly/pages/index.js",
+    "revision": "342e442381df9ac142466e3a05497ba2"
+  },
+  {
+    "url": "/_next/static/36au9bOQHhHNjShwqe0Ly/pages/quotes.js",
+    "revision": "1dd196e9ab471cddfef0e65473ec6c2d"
+  },
+  {
+    "url": "/_next/static/36au9bOQHhHNjShwqe0Ly/pages/resume.js",
+    "revision": "fb0d487be024179bd9cd9cae63e3178f"
+  },
+  {
+    "url": "/_next/static/36au9bOQHhHNjShwqe0Ly/pages/sfia-skills.js",
+    "revision": "9a5e3533e9003193967951f127fc3b30"
+  },
+  {
+    "url": "/_next/static/36au9bOQHhHNjShwqe0Ly/pages/tech-skills.js",
+    "revision": "090149b3e872479873487fa604a10ae6"
+  },
+  {
+    "url": "/_next/static/36au9bOQHhHNjShwqe0Ly/pages/timeline.js",
+    "revision": "0546185f5d1a9abb7631bf01de5f484f"
+  },
+  {
+    "url": "/_next/static/chunks/8f7c4d8ce2c7df4cc0bf160d616449143ff9a01f.ed9d87f80730ddce5554.js",
+    "revision": "c626393d0815693bc57f8636ff4844a7"
+  },
+  {
+    "url": "/_next/static/chunks/framework.c8d289f8b96717e32d7a.js",
+    "revision": "f5aa0662c1046db91fee1a246690982b"
+  },
+  {
+    "url": "/_next/static/runtime/main-c813c3cd4130941a05f5.js",
+    "revision": "763cd91ff542e0e565f9d09ffa2441c1"
+  },
+  {
+    "url": "/_next/static/runtime/polyfills-19c51f1d429240f27e5c.js",
+    "revision": "f6832bdfcecd382f46a3d81235590c4d"
+  },
+  {
+    "url": "/_next/static/runtime/webpack-c212667a5f965e81e004.js",
+    "revision": "f5e6e2fca3144cc944812cfa3547f475"
+  },
+  {
+    "url": "/404.html",
+    "revision": "200cd3069d8b6554cbd7ace3a54e42ab"
+  },
+  {
+    "url": "/index.html",
+    "revision": "76d67489458118151faf1200be7789c9"
+  },
+  {
+    "url": "/quotes.html",
+    "revision": "7ac7856177e8cf261d4b35ede4497dcd"
+  },
+  {
+    "url": "/resume.html",
+    "revision": "8e6b098aadca9433da6261424245db23"
+  },
+  {
+    "url": "/sfia-skills.html",
+    "revision": "e77854529b5ce4c6f489fae8d78fb51a"
+  },
+  {
+    "url": "/static/css/hamburgers.css",
+    "revision": "2aeedfb434a3ba8aa3dc175010f1bbf6"
+  },
+  {
+    "url": "/static/images/hashtag.png",
+    "revision": "91193c45780ff1060ccaadb81e54d2ec"
+  },
+  {
+    "url": "/static/images/hashtag.webp",
+    "revision": "b67a94418a2279fc3679f8a50bc6c356"
+  },
+  {
+    "url": "/static/images/i_menu.png",
+    "revision": "1ee97ab836e4d5073034883702b7ca51"
+  },
+  {
+    "url": "/static/images/logo_white.png",
+    "revision": "ee6cac2e4c4d1fe6daf29c216cee09d0"
+  },
+  {
+    "url": "/static/images/logo.png",
+    "revision": "e9a56e79ac434368ff632af193a3e3da"
+  },
+  {
+    "url": "/static/images/ogimage.png",
+    "revision": "92ea39600713234443ae29a0421956d0"
+  },
+  {
+    "url": "/tech-skills.html",
+    "revision": "36883ee9e7317a659833b6b1c7c53911"
+  },
+  {
+    "url": "/timeline.html",
+    "revision": "3224c078fc281521f182d716d5b132da"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
